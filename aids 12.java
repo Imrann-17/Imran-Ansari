d14a3a1f8e24dcfaa351643344e0d9d08e192e92
Imrann-17 <imranansari18269@gmail.com>
2026-07-15 12:03:17 +0530

    Add files via upload

---
+++ aids 12.java
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(){
+
+    int max,size,i,x;
+    int count;
+    int fp=-1; //first position
+    arr [n]
+    ptintf("Enter the size of array: ");
+    scanf("%d",&size);
+
+    printf("Enter array elements:");
+    for (i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+}
+
+printf("Enter number to search");
+scanf("%d",&x);
+
+for (i=0; i< size; i++){
+    if (a=[i]==x) {
+        count++;
+
+        if (fp==-1){
+            fp=i;
+        
+        }
+
+    }
+}
+
+if (count>0){
+    printf("Number found \n",x,count);
+    printf("first position=%d\n");
+
+}
+else{
+    printf("Number not founf\n");
+
+}
+ return0;
